d53298ad29fb6b62cd4407b4acb0f14e4e61c04a
BillStephens2022 <two4onebill@yahoo.com>
2023-12-10 15:53:40 -0500

    added functionality to store token on device to keep user logged in, added screenshots to be used in README

--- store/auth-context.js
+++ store/auth-context.js
@@ -1,29 +1,31 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const AuthContext = createContext({
-    token: "",
-    isAuthenticated: false,
-    authenticate: (token) => {},
-    logout: () => {},
+  token: "",
+  isAuthenticated: false,
+  authenticate: (token) => {},
+  logout: () => {},
 });
 
 function AuthContextProvider({ children }) {
-    const [authToken, setAuthToken] = useState();
+  const [authToken, setAuthToken] = useState();
 
-    function authenticate(token) {
-        setAuthToken(token);
-    }
-    function logout() {
-        setAuthToken(null);
-    }
-    const value = {
-        token: authToken,
-        isAuthenticated: !!authToken,
-        authenticate: authenticate,
-        logout: logout,
-    }
-    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-
+  function authenticate(token) {
+    setAuthToken(token);
+    AsyncStorage.setItem("token", token);
+  }
+  function logout() {
+    setAuthToken(null);
+    AsyncStorage.removeItem("token");
+  }
+  const value = {
+    token: authToken,
+    isAuthenticated: !!authToken,
+    authenticate: authenticate,
+    logout: logout,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
 export default AuthContextProvider;
